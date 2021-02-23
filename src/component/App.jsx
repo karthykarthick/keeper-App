e539d3feb07bac01430fd91f195109f3c28df81c
karthick <karthylock@gmail.com>
2021-02-23 22:46:22 +0530

    refactored the code using ES6

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
-function App () {
-    return <div>
-        <Header />
-        <Note />
-        <Footer />
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(
+        ((notesItem) => {
+          return (
+            <Note
+              key={notesItem.key}
+              title={notesItem.title}
+              content={notesItem.content}
+            />
+          );
+        })
+      )}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
