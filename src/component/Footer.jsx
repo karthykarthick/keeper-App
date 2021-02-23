6268171b8a0c8ac4f6956017070dfb43cebc3e19
karthick <karthylock@gmail.com>
2021-02-23 17:03:58 +0530

    initiated keeper project

---
+++ src/component/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const currentYear = new Date().getFullYear();
+function Footer(){
+    return <div>
+        <footer> <p>  Copyright © {currentYear} </p></footer>
+    </div>
+}
+
+export default Footer;
